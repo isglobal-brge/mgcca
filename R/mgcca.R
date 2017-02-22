a483b6eafa595daeed1766c8015dbdaa1a2de5c2
isglobal-brge <juanr.gonzalez@isglobal.org>
2017-02-22 09:15:54 +0100

    removed 'assign'

--- R/mgcca.R
+++ R/mgcca.R
@@ -1,21 +1,22 @@
-# input x (list of matrices)
+# input x (list of matrices - NOTE: each matrix should have the ids in the rownames 
 # NOTE: missing values of some variables should be imputed (to be investigated)
 
 mgcca <- function(x, mc.cores=1, ...) {
   n<-length(x) # number of tables
   
-  for (i in 1:n) assign(paste0("X", i), x[[i]])
+  if (!is.list(x))
+    stop("x must be a list containing the different matrices")
   
   rn <- NULL
   for (i in 1:n){
-  rn<- c(rn, rownames(get(paste0("X", i)))) 
+   rn<- c(rn, rownames(x[[i]])) 
   }
   rn<-sort(unique(rn))
   m<-length(rn)  # get the maximum number of individuals 
   
   X <- K <- vector("list", n)
   for (i in 1:n){
-    temp <- getK(get(paste0("X", i)), ids=rn, m=m)
+    temp <- getK(x[[i]], ids=rn, m=m)
     X[[i]] <- as.matrix(temp$X)
     K[[i]] <- temp$K
   }
