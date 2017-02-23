f4e44bda0ec28454f9cf8e82daddb6c9395b64f8
isglobal-brge <juanr.gonzalez@isglobal.org>
2017-02-23 13:57:40 +0100

    cpp changes

--- R/rcpp_functions.R
+++ R/rcpp_functions.R
@@ -1,24 +1,24 @@
-transCpp <- "using Eigen::Map;
+transCpp <- 'using Eigen::Map;
 using Eigen::MatrixXi;
 // Map the integer matrix AA from R
 const Map <MatrixXi> A(as<Map<MatrixXi>  >(AA));
 // evaluate and return the transpose of A
 const MatrixXi      At(A.transpose());
-return wrap(At);"
+return wrap(At);'
 
 ftrans <- cxxfunction(signature(AA = "matrix"), transCpp,
                       plugin = "RcppEigen")
 
-prodCpp <- typedef Eigen::Map<Eigen::MatrixXi>  MapMati;
+prodCpp <- 'typedef Eigen::Map<Eigen::MatrixXi>  MapMati;
 const MapMati    B(as<MapMati>(BB));
 const MapMati    C(as<MapMati>(CC));
 return List::create(Named("B %*% C")         = B * C,
-                    Named("crossprod(B, C)") = B.adjoint() * C);
+                    Named("crossprod(B, C)") = B.adjoint() * C);'
 
 fprod <- cxxfunction(signature(BB = "matrix", CC = "matrix"),
                      prodCpp, "RcppEigen")
 
-crossprodCpp <- "using Eigen::Map;
+crossprodCpp <- 'using Eigen::Map;
 using Eigen::MatrixXi;
 using Eigen::Lower;
 const Map <MatrixXi> A(as<Map<MatrixXi>  >(AA));
@@ -28,7 +28,7 @@ MatrixXi            AtA(MatrixXi(n, n).setZero().
 MatrixXi            AAt(MatrixXi(m, m).setZero().
                         selfadjointView<Lower>().rankUpdate(A));
 return List::create(Named("crossprod(A)")  = AtA, 
-                    Named("tcrossprod(A)") = AAt);"
+                    Named("tcrossprod(A)") = AAt);'
 
 fcprd <- cxxfunction(signature(AA = "matrix"), crossprodCpp, "RcppEigen")
 
