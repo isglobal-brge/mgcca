70247a476da093e8ce136c5084e46c67f56eed71
isglobal-brge <juanr.gonzalez@isglobal.org>
2017-02-24 11:29:06 +0100

    pinv from pracma replaced by ginv from MASS

--- R/product.R
+++ R/product.R
@@ -1,14 +1,3 @@
-productYKX0 <- function(i, Y, K, XX) {
-  ans <- pinv(t(Y)%*%K[[i]]%*%Y)%*%t(Y)%*%K[[i]]%*%XX[[i]]
-  ans
-}
-
-productXKY0 <- function(i, XX, K, Y) {
-  ans <- pinv(t(XX[[i]])%*%K[[i]]%*%XX[[i]])%*%t(XX[[i]])%*%K[[i]]%*%Y
-  ans
-}
-
-
 productYKX <- function(i, Y, K, XX) {
   yk <- crossprod(Y, K[[i]])
   yky <- ginv(yk%*%Y)
--- R/solution.R
+++ R/solution.R
@@ -1,12 +1,6 @@
-solution0 <- function(i, XX, K) {
-  ans <- K[[i]]%*%XX[[i]]%*%pinv(t(XX[[i]])%*%K[[i]]%*%XX[[i]])%*%t(XX[[i]])%*%K[[i]]
-  ans
-}
-
-
 solution <- function(i, XX, K) {
   xk <- crossprod(XX[[i]],K[[i]])
-  xkx <- pinv(xk%*%XX[[i]])
+  xkx <- ginv(xk%*%XX[[i]])
   ans <- K[[i]]%*%XX[[i]]%*%xkx%*%xk
   ans
 }
