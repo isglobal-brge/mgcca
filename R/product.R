9f9c5c892c993d6d218722d612913359e32a2b52
isglobal-brge <juanr.gonzalez@isglobal.org>
2017-02-24 09:49:38 +0100

    use of crossprod function

--- R/product.R
+++ R/product.R
@@ -1,9 +1,27 @@
-productYKX <- function(i, Y, K, XX) {
+productYKX0 <- function(i, Y, K, XX) {
   ans <- pinv(t(Y)%*%K[[i]]%*%Y)%*%t(Y)%*%K[[i]]%*%XX[[i]]
   ans
 }
 
-productXKY <- function(i, XX, K, Y) {
+productXKY0 <- function(i, XX, K, Y) {
   ans <- pinv(t(XX[[i]])%*%K[[i]]%*%XX[[i]])%*%t(XX[[i]])%*%K[[i]]%*%Y
   ans
 }
+
+
+productYKX <- function(i, Y, K, XX) {
+  yk <- crossprod(Y, K[[i]])
+  yky <- pinv(yk%*%Y)
+  ykx <- yk%*%XX[[i]]
+  ans <- yky%*%ykx
+  ans
+}
+
+
+productXKY <- function(i, XX, K, Y) {
+   xk <- crossprod(XX[[i]], K[[i]])
+   xkx <- pinv(xk%*%XX[[i]])
+   xky <- xk%*%Y
+   ans <- xkx%*%xky
+   ans
+  }
--- R/solution.R
+++ R/solution.R
@@ -1,4 +1,12 @@
-solution <- function(i, XX, K) {
+solution0 <- function(i, XX, K) {
   ans <- K[[i]]%*%XX[[i]]%*%pinv(t(XX[[i]])%*%K[[i]]%*%XX[[i]])%*%t(XX[[i]])%*%K[[i]]
+  ans
+}
+
+
+solution <- function(i, XX, K) {
+  xk <- crossprod(XX[[i]],K[[i]])
+  xkx <- pinv(xk%*%XX[[i]])
+  ans <- K[[i]]%*%XX[[i]]%*%xkx%*%xk
   ans
 }
