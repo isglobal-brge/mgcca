8e14fb8738ed68d5c7d5c6a10a6bfe64e3f6d196
isglobal-brge <juanr.gonzalez@isglobal.org>
2017-02-24 10:16:47 +0100

    ginv used instead of pinv

--- R/product.R
+++ R/product.R
@@ -11,7 +11,7 @@ productXKY0 <- function(i, XX, K, Y) {
 
 productYKX <- function(i, Y, K, XX) {
   yk <- crossprod(Y, K[[i]])
-  yky <- pinv(yk%*%Y)
+  yky <- ginv(yk%*%Y)
   ykx <- yk%*%XX[[i]]
   ans <- yky%*%ykx
   ans
@@ -20,7 +20,7 @@ productYKX <- function(i, Y, K, XX) {
 
 productXKY <- function(i, XX, K, Y) {
    xk <- crossprod(XX[[i]], K[[i]])
-   xkx <- pinv(xk%*%XX[[i]])
+   xkx <- ginv(xk%*%XX[[i]])
    xky <- xk%*%Y
    ans <- xkx%*%xky
    ans
