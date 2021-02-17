659fb3196e66eb54cb68ba22cd01f5b324dd7689
dpelegri <dpelegri@gmail.com>
2021-02-17 19:31:10 +0100

    Update getWeights_bd.R

--- R/getWeights_bd.R
+++ R/getWeights_bd.R
@@ -1,6 +1,6 @@
 getWeights <- function(i, A, XX, K){
 
-  KXA <- bblockmult(blockmult(K[[i]],XX[[i]]),A[[i]], onmemory = T)
+  KXA <- blockmult(blockmult(K[[i]],XX[[i]]),A[[i]], onmemory = T)
   vv <- 1/apply(KXA, 2, sd)
   vv <-  blockmult(cbind(rep(1,nrow(A[[i]]))),rbind(vv) , onmemory = T)
   As <- blockmult(A[[i]],vv, onmemory = T)
