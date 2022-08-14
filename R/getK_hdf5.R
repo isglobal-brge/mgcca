d8ec070c45dd0fa0e24cbbe1bcf564fc78c0823e
dpelegri <dpelegri@gmail.com>
2022-08-14 21:28:04 +0200

    Update getK_hdf5.R

--- R/getK_hdf5.R
+++ R/getK_hdf5.R
@@ -2,28 +2,47 @@ getK_hdf5 <- function(x, ids, m, filename, group) {
 
     outgroup = paste0(group, "_out")
     dimnames <- getDimNames_hdf5(filename, group, x)
-    na.ids <- ids[ !ids %in% t(dimnames$rownames) ]
+    na.ids <- ids[ !ids %in% t(dimnames$rownames)]
+
+
+    if( !bdExists_hdf5_element(filename, "tmp_mgga") ) {
+      bdCreateGroup_hdf5( filename, "tmp_mgga" )
+    }
+
 
     if( length(na.ids) == 0 ) {
 
-        X <- cbind(dimnames[[1]], order = seq(1:(nrow(dimnames[[1]]) + length(na.ids))))
+
+        X <- cbind( dimnames[[1]],
+                    order = seq(1:(nrow(dimnames[[1]]) + length(na.ids))),
+                    diag = c(rep(1,nrow(dimnames[[1]]))) )
         rownames(X) <- X[,1]
         X <- X[ids,]
         X$newOrder = seq( 1:nrow(X))
-        blocks <- split( X, cumsum(c(TRUE, diff(X$order) != 1)) )
+        blocks <- split(X, cumsum(c(TRUE, diff(X$order) != 1)))
 
-        sapply( 1:length(blocks), function(x) {
-          bdAdd_hdf5_matrix(blocks[x], filename = filename, group = group, dataset = names(blocks[x]))
-        } )
+        BigDataStatMeth::bdCreateEmptyDataset_hdf5(
+            filename = filename, group = "X", dataset = x,
+            nrows = nrow(dimnames[[2]]), ncols = nrow(dimnames[[1]]),
+            overwrite = T )
+        bdSort_hdf5_dataset( filename = filename, group = group, dataset = x,
+                             outdataset = x, blockedSortlist = blocks,
+                             func = "sortRows", outgroup = "X" )
 
-        browser()
-
-        BigDataStatMeth::bdBind_hdf5( filename, group, bdgetDatasetsList_hdf5(filename, group), "X", outdataset = x, func = "bindRows")
         BigDataStatMeth::bdCreateEmptyDataset_hdf5(filename, "K", x, nrow(X), nrow(X) )
+        ##
+        ## ==>> TO DO :
+        ## ==>> AQUÍ FALTARÀ AFEGIR EL DIMNAMES I EL COLNAMES!!! A LA NOVA MATRIU X !!!!
+        ## ==>> PERQUÈ SINÓ NO HO TINDREM ORDENAT !!!!
+        ##
         BigDataStatMeth::bdWriteDiagonal_hdf5( rep(1, nrow(X)), filename, "K", x)
 
     } else {
 
+      ##
+      ## ==>> TO DO :
+      ## ==>> REVISAR TOTA AQUESTA PART !!!!
+      ##
         X <- cbind( rbind(dimnames[[1]], na.ids),
                     order = seq(1:(nrow(dimnames[[1]]) + length(na.ids))),
                     diag = c(rep(1,nrow(dimnames[[1]])), rep(0, length(na.ids))) )
