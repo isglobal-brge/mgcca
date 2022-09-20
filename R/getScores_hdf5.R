9c03c232bdaa1d49548d59766a4e440bcdea0c54
dpelegri <dpelegri@gmail.com>
2022-09-20 20:40:14 +0200

    Update getScores_hdf5.R

--- R/getScores_hdf5.R
+++ R/getScores_hdf5.R
@@ -1,11 +1,12 @@
 getScores_hdf5 <- function(filename, XX, As, threads) {
 
+    grp_score <- "FINAL_RESULTS/scores"
     bdapply_Function_hdf5(filename = filename,
                           group = "X", datasets = XX,
                           b_group = "scores/As", b_datasets = As,
-                          outgroup = "scores/scores", func = "blockmult",transp_dataset = T,
+                          outgroup = grp_score, func = "blockmult",transp_dataset = T,
                           force = T)
-    scores <- bdgetDatasetsList_hdf5(filename = filename, group = "scores")
+    scores <- bdgetDatasetsList_hdf5(filename = filename, group = grp_score)
     return(scores)
 
 }
