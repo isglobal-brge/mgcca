6eedbb49f8baafa7af355e6dda360df148fc99a6
dpelegri <dpelegri@gmail.com>
2022-09-20 20:40:04 +0200

    Update getCor_hdf5 to work with incomplete data

--- R/getCor_hdf5.R
+++ R/getCor_hdf5.R
@@ -1,21 +1,24 @@
-getCor_hdf5 <- function(filename, x, y, byblocks, threads) {
+getCor_hdf5 <- function(filename, Xgroup, x, Ygroup, y, byblocks, threads) {
 
-    maxelements <- 300
+    maxelements <- 30000
+
+    common <- BigDataStatMeth::bdgetDiagonal_hdf5(filename, group = "K", dataset =  x)
+    common_elems <- which(common!=0)
+
+    if (!inherits(y, "matrix") && !inherits(y, "data.frame")) {
+        if(inherits(y,"character")){
+            Y <- rhdf5::h5read(filename,y, index = list(common_elems, NULL))
+        }
+    } else {
+        if(inherits(y, "data.frame")) {
+            Y <- as.matrix(y[common_elems,])
+        } else {
+            Y <- y[common_elems,]
+        }
+    }
 
     if (byblocks) {
-            dims <- BigDataStatMeth::bdgetDim_hdf5(filename,x)
-
-            if (!inherits(y, "matrix") && !inherits(y, "data.frame")) {
-                if(inherits(Y,"character")){
-                    Y <- rhdf5::h5read(filename,y)
-                }
-            } else {
-                if(inherits(y, "data.frame")) {
-                    Y <- as.matrix(y)
-                } else {
-                    Y <- y
-                }
-            }
+            dims <- BigDataStatMeth::bdgetDim_hdf5(filename, paste0(Xgroup,"/", x))
 
             if(dims[1] < maxelements) { sizeblock <- maxelements / dims[1]
             } else { sizeblock <- 1 }
@@ -23,14 +26,18 @@ getCor_hdf5 <- function(filename, x, y, byblocks, threads) {
             xg <- split(1:dims[1], ceiling(seq_along(1:dims[1])/sizeblock))
 
             res <- lapply(xg, function(dat, mY) {
-                X <- rhdf5::h5read(filename, x, index = list(dat, NULL))
+                X <- rhdf5::h5read(filename, paste0(Xgroup,"/",x), index = list(dat, common_elems))
                 cor(t(X),mY)
             }, mY = Y)
-            return(do.call(rbind,res))
+            res <- do.call(rbind,res)
 
     } else {
-        return(cor(X,Y))
+        X <- rhdf5::h5read(filename, paste0(Xgroup,"/",x),
+                           index = list(NULL, common_elems))
+        res <- cor(X,Y)
     }
 
+    rownames(res) <- t(getDimNames_hdf5(filename, Xgroup, x)$colnames)
+    return(res)
 }
 
